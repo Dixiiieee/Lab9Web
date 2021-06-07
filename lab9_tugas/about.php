c913765c34db9340ef007133ffe45afef2f9de84
Sigit Ardiansyah <56240134+Dixiiieee@users.noreply.github.com>
2021-06-07 20:39:59 +0700

    Add files via upload

---
+++ lab9_tugas/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+
+<div class="content">
+    <h2>Sigit Ardiansyah</h2>
+    <p1>Ini adalah bagian content dari halaman about.</p1>
+</div>
+<br>
+<?php require('footer.php'); ?>
---
+++ lab9_tugas/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; 2021, Sigit Ardiansyah, 311910627, TI.19.A.2 Informatika, Universitas Pelita Bangsa</p>
+    </footer>
+    </div>
+</body>
+</html>
